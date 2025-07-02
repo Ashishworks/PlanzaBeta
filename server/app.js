a109ebde8d4543b22002f953527ceee8329a0f8d
new-username <ashish061104@gmail.com>
2025-07-02 19:43:25 +0530

    env setup

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/generate-mermaid", geminiRouter);
 
-const PORT =5000;
+const PORT =5000 || process.env.PORT
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
 );
